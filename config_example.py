7c15e50cde21fe4a222682623be99bd1f4b85652
a.gridnev <anton@gridnev.pro>
2022-07-06 14:31:27 +1100

    housekeeping

--- config_example.py
+++ config_example.py
@@ -8,7 +8,10 @@ TOKEN = ""
 ###
 config_file = None
 logging_file = None
+### Activate local login ###
+local_login = True
 ### LDAP Settings ###
+# If you need ldap (AD) login, you should change local_login to False
 AD_ADDRESS = ""
 AD_SEARCH_TREE = ""
 # If you have a problem with changing only the clock period in your configs, enable fix_clock_period
