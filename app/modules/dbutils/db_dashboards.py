b371f1d129cee2340ec87da31f5a230644990729
a.gridnev <anton@gridnev.pro>
2023-09-06 14:48:54 +1100

    Add Chart.js

--- app/modules/dbutils/db_dashboards.py
+++ app/modules/dbutils/db_dashboards.py
@@ -53,7 +53,7 @@ def get_models_count(user_id: int) -> dict or bool:
     This function needs to get allowed models count
     """
     if not isinstance(user_id, int) or user_id is None:
-        logger.info(f"Get models count for {user_id} error, device id is not a integer")
+        logger.info(f"Get models count for {user_id} error, user id is not a integer")
         return False
     try:
         slq_request = text(
@@ -89,7 +89,7 @@ def get_configs_count(user_id: int) -> dict or bool:
     This function needs to get allowed config count
     """
     if not isinstance(user_id, int) or user_id is None:
-        logger.info(f"Get config count for {user_id} error, device id is not a integer")
+        logger.info(f"Get config count for {user_id} error, user id is not a integer")
         return False
     try:
         slq_request = text(
@@ -119,3 +119,43 @@ def get_configs_count(user_id: int) -> dict or bool:
         # If an error occurs as a result of writing to the DB,
         # then rollback the DB and write a message to the log
         logger.info(f"Get configs count on devices error {get_sql_error}")
+
+
+def get_error_connections(user_id: int) -> list or bool:
+    """
+    This function needs to get last 10 error connection
+    """
+    if not isinstance(user_id, int) or user_id is None:
+        logger.info(f"Get error connection for {user_id} error, user id is not a integer")
+        return False
+    try:
+        slq_request = text(
+            """
+            SELECT connection_status, 
+                device_id, 
+                device_ip,
+                device_hostname,  
+                device_vendor, 
+                timestamp 
+            FROM Associating_Device 
+            LEFT JOIN Devices ON Devices.id = Associating_Device.device_id 
+            LEFT JOIN group_permission ON group_permission.user_group_id = Associating_Device.user_group_id 
+            WHERE group_permission.user_id = :user_id AND Devices.connection_status <> 'Ok' 
+            ORDER BY timestamp desc 
+            limit 10;
+            """
+        )
+        parameters = {"user_id": user_id}
+        request_data = db.session.execute(slq_request, parameters).fetchall()
+        return [{
+            "html_element_count": html_element_count,
+            "connection_status": i["connection_status"],
+            "device_ip": i["device_ip"],
+            "device_hostname": i["device_hostname"],
+            "device_vendor": i["device_vendor"],
+            "timestamp": i["timestamp"],
+        } for html_element_count, i in enumerate(request_data, start=1)]
+    except Exception as get_sql_error:
+        # If an error occurs as a result of writing to the DB,
+        # then rollback the DB and write a message to the log
+        logger.info(f"Get error connection on devices error {get_sql_error}")
