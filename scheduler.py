b8b03583a18275ab47d9003073b3f7332d7cd5e0
a.gridnev <a.gridnev@sakhalin.gov.ru>
2026-04-13 18:01:39 +1100

    feat: built-in scheduler

--- scheduler.py
+++ scheduler.py
@@ -3,6 +3,7 @@ import atexit
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
 
+from app import logger
 
 _background_scheduler = None
 
@@ -21,11 +22,13 @@ def get_scheduler():
 
 
 def scheduled_backup():
+    logger.info("=== scheduled_backup started ===")
     from backuper import run_backup
     sched = get_scheduler()
     if sched and hasattr(sched, 'app'):
         with sched.app.app_context():
             run_backup()
+    logger.info("=== scheduled_backup finished ===")
 
 
 def shutdown_scheduler():
