b5adc8c7e28b9a92b9e07c899b9d3202269bc907
a.gridnev <a.gridnev@sakhalin.gov.ru>
2026-07-21 12:39:24 +1100

    fix(security): critical security patches v2.5.3

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_compress import Compress
+from flask_session import Session
 from flask_wtf import CSRFProtect
 from werkzeug.middleware.proxy_fix import ProxyFix
 
@@ -29,6 +30,7 @@ logger = setup_logging(log_level="INFO")
 app = Flask(__name__)
 # app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+Session(app)
 Compress(app)
 # Add config parameters in flask app and chose release
 app.config.from_object(f"app.configuration.{release_options}")
