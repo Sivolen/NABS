231e4425232ea62ccac251a6391a35e702c2c5d3
a.gridnev <a.gridnev@sakhalin.gov.ru>
2026-07-21 13:35:28 +1100

    fix(security): critical security patches v2.5.3

--- app/configuration.py
+++ app/configuration.py
@@ -1,4 +1,4 @@
-from config import TOKEN, DBHost, DBPort, DBName, DBUser, DBPassword, NABS_BASE_URL
+from config import TOKEN, DBHost, DBPort, DBName, DBUser, DBPassword, NABS_DOMAIN
 
 
 class Config(object):
@@ -45,7 +45,7 @@ class ProductionConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_COOKIE_SECURE = True
     PERMANENT_SESSION_LIFETIME = 86400
-    SESSION_COOKIE_DOMAIN = NABS_BASE_URL
+    SESSION_COOKIE_DOMAIN = NABS_DOMAIN
     # PREFERRED_URL_SCHEME = 'https'
     SESSION_FILE_DIR = '/tmp/flask_sessions'
 
