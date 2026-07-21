d60a6292438e9432cefd4af30f8b958c28fe9ed0
a.gridnev <a.gridnev@sakhalin.gov.ru>
2026-07-21 14:02:26 +1100

    fix(security): critical security patches v2.5.3

--- app/views/auth.py
+++ app/views/auth.py
@@ -10,6 +10,7 @@ from app.modules.auth.auth_users_local import AuthUsers
 from app.modules.auth.auth_users_ldap import LdapFlask
 from urllib.parse import urlparse, urljoin
 import logging
+from flask_wtf.csrf import generate_csrf
 
 # Initialize logger
 logger = logging.getLogger(__name__)
